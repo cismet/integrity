c226ed33745bbf3889c024165ccdd925947f4525
helllth <th@cismet.de>
2013-12-17 16:48:13 +0100

    working on #3 flexible check folder name

--- lib/integrity.sh
+++ lib/integrity.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+checksfolder=$1
+if [ ! -d "$checksfolder" ]
+    then 
+        echo "No folder '$checksfolder'. Will try 'checks'."
+        checksfolder=checks
+        if [ ! -d "$checksfolder" ]
+        then 
+            echo "No folder '$checksfolder'. Will stop."
+            exit 1
+        fi
+fi
+d_echo "Checks in '$checksfolder'"
 source .integrity
 
 source $integrity_home/lib/parts/init.part
